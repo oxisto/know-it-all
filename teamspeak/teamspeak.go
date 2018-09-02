f0488d177a0c7cda016c57ffec5a0929d6522a5e
Christian Banse <oxisto@aybaze.com>
2018-09-02 22:59:12 +0200

    maybe fixed ts

--- teamspeak/teamspeak.go
+++ teamspeak/teamspeak.go
@@ -15,6 +15,9 @@ var tsClient *ts3.Client
 
 var users map[int]string
 
+var ticker *time.Ticker
+var quit chan struct{}
+
 func Init(address string, username string, password string) {
 	var err error
 
@@ -46,77 +49,114 @@ func Init(address string, username string, password string) {
 
 func ListenForEvents() {
 	// keep alive
-	ticker := time.NewTicker(5 * time.Minute)
-	quit := make(chan struct{})
+	ticker = time.NewTicker(3 * time.Minute)
+	quit = make(chan struct{})
 
 	tsClient.NotifyHandler(NotifyHandler)
+	tsClient.ConnErrorHandler(ConnErrorHandler)
 	tsClient.ExecString("use 1")
 	tsClient.ExecString("servernotifyregister event=server")
 
+	if err := UpdatePlayerList(); err != nil {
+		log.Printf("An error occured while executing ts3 command: %v\n", err)
+		quit <- struct{}{}
+	}
+
+test:
 	for {
 		select {
 		case <-ticker.C:
 			// do stuff
-			resp, err := tsClient.Exec(ts3.ClientList())
-			if err == nil {
-				log.Printf("Updating client list...\n")
-				for _, client := range resp.Params {
-					clientID, err := strconv.Atoi(client["clid"])
-					if err != nil {
-						log.Printf("Could not parse clid %s: %v\n", client["clid"], err)
-						continue
-					}
-					users[clientID] = client["client_nickname"]
-				}
-				log.Printf("%v\n", resp)
-			} else {
+			if err := UpdatePlayerList(); err != nil {
 				log.Printf("An error occured while executing ts3 command: %v\n", err)
 				quit <- struct{}{}
 			}
 		case <-quit:
 			ticker.Stop()
 			log.Printf("Stopping connection to TS3 server...\n")
-			break
+			break test
 		}
 	}
 
 	log.Printf("Connection to TS server was lost.\n")
 }
 
-func NotifyHandler(n ts3.Notification) {
-	log.Printf("%v\n", n)
-
-	if n.Type == "notifycliententerview" {
-		nickname := n.Params[0]["client_nickname"]
-		clientID, err := strconv.Atoi(n.Params[0]["clid"])
-		if err != nil {
-			log.Printf("Could not parse clid %s: %v\n", clientID, err)
-			return
+func UpdatePlayerList() error {
+	resp, err := tsClient.Exec(ts3.ClientList())
+	if err == nil {
+		log.Printf("Updating client list...\n")
+		for _, client := range resp.Params {
+			// don't post regular user updates to slack, only notification
+			OnUserEnter(client, false)
 		}
+		log.Printf("Response from ts3.ClientList(): %v\n", resp)
+		return nil
+	}
 
+	return err
+}
+
+func OnUserEnter(client map[string]string, postToSlack bool) {
+	clientType, err := strconv.Atoi(client["client_type"])
+	if err != nil {
+		log.Printf("Could not parse client_type %s: %v\n", client["client_type"], err)
+		return
+	}
+
+	if clientType != 0 {
+		return
+	}
+
+	nickname := client["client_nickname"]
+	clientID, err := strconv.Atoi(client["clid"])
+	if err != nil {
+		log.Printf("Could not parse clid %s: %v\n", client["clid"], err)
+		return
+	}
+
+	log.Printf("Adding %s to teamspeak user list.", nickname)
+	users[clientID] = nickname
+
+	if postToSlack {
 		// send to Slack
 		params := slack.PostMessageParameters{}
 		params.AsUser = true
 
 		// TODO: get channel id from somewhere
 		bot.SendMessage("#general", fmt.Sprintf("%s joined TS3", nickname), params)
+	}
+}
 
-		// add to users
-		users[clientID] = nickname
+func OnUserLeave(client map[string]string, postToSlack bool) {
+	clientID, err := strconv.Atoi(client["clid"])
+	if err != nil {
+		log.Printf("Could not parse clid %s: %v\n", client["clid"], err)
+		return
+	}
+
+	nickname := users[clientID]
+	if nickname != "" && postToSlack {
+		// send to Slack
+		params := slack.PostMessageParameters{}
+		params.AsUser = true
+
+		bot.SendMessage("#general", fmt.Sprintf("%s left TS3", nickname), params)
+	}
+}
+
+func ConnErrorHandler(err error) {
+	log.Printf("Error: %v", err)
+
+	// stop the ticker
+	quit <- struct{}{}
+}
+
+func NotifyHandler(n ts3.Notification) {
+	log.Printf("%v\n", n)
+
+	if n.Type == "notifycliententerview" {
+		OnUserEnter(n.Params[0], true)
 	} else if n.Type == "notifyclientleftview" {
-		clientID, err := strconv.Atoi(n.Params[0]["clid"])
-		if err != nil {
-			log.Printf("Could not parse clid %s: %v\n", n.Params[0]["clid"], err)
-			return
-		}
-
-		nickname := users[clientID]
-		if nickname != "" {
-			// send to Slack
-			params := slack.PostMessageParameters{}
-			params.AsUser = true
-
-			bot.SendMessage("#general", fmt.Sprintf("%s left TS3", nickname), params)
-		}
+		OnUserLeave(n.Params[0], true)
 	}
 }
